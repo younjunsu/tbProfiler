4f6301e9683d6383fe17254245e5b196e3407f19
younjunsu <junsu_youn@naver.com>
2023-02-09 14:41:12 +0900

    Update tuning.sql

--- reference_sql_tuning/tuning.sql
+++ reference_sql_tuning/tuning.sql
@@ -210,28 +210,6 @@ Execution Stat
    1  ORDER BY (SORT) TOP-N (Time:527.26 ms, Rows:10, Starts:1) 
    2    TABLE ACCESS (FULL): USER_POST_INFO (Time:273.56 ms, Rows:3000000, Starts:1) 
 
-select
-    post_view_top.post_id,
-    post_view_top.post_view
-from
-    (
-        select /*+ INDEX (user_post_info idx_post_info_post_view ) */
-            post_id,
-            post_view
-        from
-            user_post_info
-    --    where
-            -- post_id is not null
-        order by 
-            post_view desc
-    ) post_view_top
-where
-    rownum <=10;
-
-create index idx_post_info_post_id on user_post_info(post_id);
-create index idx_post_info_post_view on user_post_info(post_view);
-
-
 
 -- 게시글 좋아요를 가장 많이 받은 게시글 탑 10개
 select
@@ -245,9 +223,7 @@ from
         from
             user_post_info
         where
-            -- post_id is not null
-            post_id > 0 --and
-            --post_like > 0
+            post_id is not null
         order by
             post_like desc
     ) post_like_top
@@ -258,17 +234,6 @@ Execution Stat
 ----------------------------------------------------------------------------------------------------
    1  ORDER BY (SORT) TOP-N (Time:536.8 ms, Rows:10, Starts:1) 
    2    TABLE ACCESS (FULL): USER_POST_INFO (Time:282.95 ms, Rows:3000000, Starts:1) 
-
-create index idx_user_post_info_001 on user_post_info(post_like, post_id);
-drop index idx_user_post_info_001 ;
-
-
-Execution Stat
-----------------------------------------------------------------------------------------------------
-   1  ORDER BY (SORT) TOP-N (Time:439.89 ms, Rows:10, Starts:1) 
-   2    FILTER (Time:87.73 ms, Rows:3000000, Starts:1) 
-   3      INDEX (FAST FULL SCAN): IDX_USER_POST_INFO_001 (Time:43.31 ms, Rows:3000000, Starts:1) 
-
 
 -- 댓글 좋아요를 가장 많이 받은 게시글 탑 10개
 select
@@ -293,29 +258,6 @@ Execution Stat
 ----------------------------------------------------------------------------------------------------
    1  ORDER BY (SORT) TOP-N (Time:1095.78 ms, Rows:10, Starts:1) 
    2    TABLE ACCESS (FULL): USER_COMMENT_INFO (Time:2037.77 ms, Rows:5000000, Starts:1) 
-
-
-select
-    comment_like_top.post_id,
-    comment_like_top.comment_like
-from
-    (
-        select
-            post_id,
-            comment_like
-        from
-            user_comment_info
-        where
-            post_id > -1111
-            and comment_like > -111
-        order by
-            comment_like desc
-    ) comment_like_top
-where
-    rownum <= 10;
-
-
-create index idx_comment_info_001 on user_comment_info(comment_like, post_id);
 
 
 -- 게시판 페이지에 최신 작성 게시글 10개씩 출력
@@ -350,72 +292,7 @@ Execution Stat
    2    ORDER BY (SORT) TOP-N (Time:664.2 ms, Rows:10, Starts:1) 
    3      TABLE ACCESS (FULL): USER_POST_INFO (Time:65.89 ms, Rows:3000000, Starts:1) 
 
-
-select
-    post_createdate_top.no,
-    post_createdate_top.post_id,
-    post_createdate_top.post_path,
-    post_createdate_top.post_createdate    
-from 
-    (
-        select
-            rownum no,
-            post_createdate_top.*
-        from
-            (
-                select /*+ INDEX (user_post_info IDX_USER_POST_INFO_002) */
-                    post_id,
-                    post_path,
-                    post_createdate 
-                from
-                    user_post_info
-                where
-                    post_createdate > to_date('19700101', 'yyyymmdd')
-                order by 
-                    post_createdate desc
-            ) post_createdate_top
-    ) post_createdate_top
-where
-    post_createdate_top.no between 1 and 10;
-
-create index IDX_USER_POST_INFO_002 on user_post_info (post_createdate);
-
--- 게시판 페이지에 최신 작성 게시글 30개씩 출력
-select
-    post_createdate_top.no,
-    post_createdate_top.post_id,
-    post_createdate_top.post_path,
-    post_createdate_top.post_createdate    
-from 
-    (
-        select
-            rownum no,
-            post_createdate_top.*
-        from
-            (
-                select /*+ INDEX (user_post_info IDX_USER_POST_INFO_002) */
-                    post_id,
-                    post_path,
-                    post_createdate 
-                from
-                    user_post_info
-                where
-                    post_createdate > to_date('19700101', 'yyyymmdd')
-                order by 
-                    post_createdate desc
-            ) post_createdate_top
-    ) post_createdate_top
-where
-    post_createdate_top.no between 101 and 120;
-
-Execution Stat
-----------------------------------------------------------------------------------------------------
-   1  FILTER (Time:.01 ms, Rows:10, Starts:1) 
-   2    ORDER BY (SORT) TOP-N (Time:680.52 ms, Rows:20, Starts:1) 
-   3      TABLE ACCESS (FULL): USER_POST_INFO (Time:69.37 ms, Rows:3000000, Starts:1) 
-
-
--- 게시판 페이지에 최신 작성 게시글 50개씩 출력
+-- 게시판 페이지에 최신 작성 게시글 10개씩 출력
 select
     post_createdate_top.no,
     post_createdate_top.post_id,
@@ -439,7 +316,39 @@ from
             ) post_createdate_top
     ) post_createdate_top
 where
-    post_createdate_top.no between 110 and 120;
+    post_createdate_top.no between 11 and 20;
+
+Execution Stat
+----------------------------------------------------------------------------------------------------
+   1  FILTER (Time:.01 ms, Rows:10, Starts:1) 
+   2    ORDER BY (SORT) TOP-N (Time:680.52 ms, Rows:20, Starts:1) 
+   3      TABLE ACCESS (FULL): USER_POST_INFO (Time:69.37 ms, Rows:3000000, Starts:1) 
+
+-- 게시판 페이지에 최신 작성 게시글 10개씩 출력
+select
+    post_createdate_top.no,
+    post_createdate_top.post_id,
+    post_createdate_top.post_path,
+    post_createdate_top.post_createdate    
+from 
+    (
+        select
+            rownum no,
+            post_createdate_top.*
+        from
+            (
+                select
+                    post_id,
+                    post_path,
+                    post_createdate 
+                from
+                    user_post_info
+                order by 
+                    post_createdate desc
+            ) post_createdate_top
+    ) post_createdate_top
+where
+    post_createdate_top.no between 21 and 30;
 
 Execution Stat
 ----------------------------------------------------------------------------------------------------
